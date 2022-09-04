8d2148cbcc4632b4561a66b3a9e6da54937b4dce
connor <connoroakvillea32@hotmail.com>
2022-09-04 16:14:18 -0400

    making base endpoints

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@ import express from 'express';
 
 const app = express();
 
+// root
 app.get('/', function(req, res) {
     res.send('Hello World');
+});
+
+// clarifai
+
+
+// POST signIn
+app.post('/signIn', function(req, res) {
+    res.send('sign in');
+});
+
+// POST signUp
+app.post('/signUp', function(req, res) {
+    res.send('sign up');
+});
+
+// GET user
+app.get('/user', function(req, res) {
+    res.send('user');
+});
+
+// UPDATE entries
+app.update('/entries', function(req, res) {
+    res.send('entries');
 });
 
 app.listen(3000);
